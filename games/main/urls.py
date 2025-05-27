12b8291302a7c12137b4d5752f93e6d75fe3cd5a
EdikN <84894638+EdikN@users.noreply.github.com>
2025-05-27 14:09:07 +0500

    genre

--- games/main/urls.py
+++ games/main/urls.py
@@ -6,5 +6,9 @@ from django.contrib import admin
 
 urlpatterns = [
     path('',views.index),
+    path('Action/',views.get_action),
+    path('Race/',views.get_race),
+    path('Finance/',views.get_finance),
+    path('Strategy/',views.get_strategy),
     path('game/<slug:game_slug>/', views.game_detail, name='game-detail')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- games/main/views.py
+++ games/main/views.py
@@ -10,3 +10,19 @@ def index(request):
 def game_detail(request,game_slug):
     games = get_object_or_404(Games, slug=game_slug)
     return render(request, 'main/game.html', {'game': games})
+
+def get_action(request):
+    games = Games.objects.filter(genre='Action')
+    return render(request, 'main/index.html', {'games': games})
+
+def get_race(request):
+    games = Games.objects.filter(genre='Race')
+    return render(request, 'main/index.html', {'games': games})
+
+def get_finance(request):
+    games = Games.objects.filter(genre='Finance')
+    return render(request, 'main/index.html', {'games': games})
+
+def get_strategy(request):
+    games = Games.objects.filter(genre='Strategy')
+    return render(request, 'main/index.html', {'games': games})
