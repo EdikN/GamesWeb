912191a99235ee241bf016c1b0751d4da32aaffa
EdikN <84894638+EdikN@users.noreply.github.com>
2025-05-27 10:30:38 +0500

    date

--- games/main/models.py
+++ games/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Games(models.Model):
@@ -6,7 +7,8 @@ class Games(models.Model):
     game_url = models.URLField('Ссылка на игру' , max_length=250 , default='')
     description = models.CharField('Описание', max_length=160 , default='')
     thumbnail= models.URLField('Url иконки', max_length=250, default='')
-    genre = models.URLField('Жанр', max_length=30, default='')
+    genre = models.CharField('Жанр', max_length=30, default='')
+    date = models.DateField('Дата разработки',default= date(2020, 10, 15))
     
     def __str__(self):
         return self.title
